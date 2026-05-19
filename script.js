0b7c652b0807e45fdb490cb666fcc4613560f346
um11rr <140225804+UmarrrrAziz@users.noreply.github.com>
2026-05-19 21:10:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -61,3 +61,108 @@ window.addEventListener("load", ()=>{
     },1200);
 
 });
+function toggleChat(){
+
+    const chat =
+    document.getElementById("jarvisChat");
+
+    if(chat.style.display === "block"){
+
+        chat.style.display = "none";
+
+    }else{
+
+        chat.style.display = "block";
+
+    }
+}
+
+function sendMessage(){
+
+    const input =
+    document.getElementById("userInput");
+
+    const chatBody =
+    document.getElementById("chatBody");
+
+    const userText =
+    input.value.toLowerCase();
+
+    if(input.value.trim() === "") return;
+
+    const userMessage =
+    document.createElement("div");
+
+    userMessage.className =
+    "user-message";
+
+    userMessage.textContent =
+    input.value;
+
+    chatBody.appendChild(userMessage);
+
+    let reply =
+    "I don't understand that yet.";
+
+    if(userText.includes("name")){
+
+        reply =
+        "His name is Umar Bin Abdul Aziz 🚀";
+
+    }
+
+    else if(userText.includes("bgmi")){
+
+        reply =
+        "BGMI ID: 5113772029 🎮";
+
+    }
+
+    else if(userText.includes("skills")){
+
+        reply =
+        "Python, Next.js, HTML, CSS, JavaScript and Gaming.";
+
+    }
+
+    else if(userText.includes("instagram")){
+
+        reply =
+        "@um11rr_ 📸";
+
+    }
+
+    else if(userText.includes("telegram")){
+
+        reply =
+        "@um11rr ✈️";
+
+    }
+
+    else if(userText.includes("coder")){
+
+        reply =
+        "Yes 😎 Umar is passionate about coding and development.";
+
+    }
+
+    const botMessage =
+    document.createElement("div");
+
+    botMessage.className =
+    "bot-message";
+
+    setTimeout(() => {
+
+        botMessage.textContent =
+        reply;
+
+        chatBody.appendChild(botMessage);
+
+        chatBody.scrollTop =
+        chatBody.scrollHeight;
+
+    }, 700);
+
+    input.value = "";
+}
