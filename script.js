ef5a0a4a8bcfc81f81338f36fc34937ea7cdd80c
um11rr <140225804+UmarrrrAziz@users.noreply.github.com>
2026-05-19 20:40:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 // script.js
 
 console.log("Portfolio Loaded Successfully!");
+window.addEventListener("load", function(){
+
+    const loader = document.getElementById("loader");
+
+    setTimeout(() => {
+
+        loader.style.opacity = "0";
+
+        loader.style.visibility = "hidden";
+
+    }, 2000);
+
+});
