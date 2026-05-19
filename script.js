2e3ca9de210a2d2324796e7b633f759c4fe17c23
um11rr <140225804+UmarrrrAziz@users.noreply.github.com>
2026-05-19 21:02:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,3 +14,50 @@ window.addEventListener("load", function(){
     }, 2000);
 
 });
+/* AI FACE SCAN */
+
+window.addEventListener("load", ()=>{
+
+    const scanner = document.getElementById("scanner");
+
+    const status = document.getElementById("scan-status");
+
+    const messages = [
+
+        "Initializing AI Systems",
+
+        "Scanning Face",
+
+        "Identity Verified",
+
+        "Access Granted",
+
+        "Welcome Umar Bin Abdul Aziz"
+
+    ];
+
+    let i = 0;
+
+    const interval = setInterval(()=>{
+
+        status.innerText = messages[i];
+
+        i++;
+
+        if(i >= messages.length){
+
+            clearInterval(interval);
+
+            setTimeout(()=>{
+
+                scanner.style.opacity = "0";
+
+                scanner.style.visibility = "hidden";
+
+            },1000);
+
+        }
+
+    },1200);
+
+});
