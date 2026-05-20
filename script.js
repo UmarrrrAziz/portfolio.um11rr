6d4697fb0b1902fff75cf2aa5fa63b932f7e0948
um11rr <140225804+UmarrrrAziz@users.noreply.github.com>
2026-05-20 10:29:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -86,5 +86,91 @@ window.addEventListener("load", () => {
     },1200);
 
 });
+async function sendMessage(){
 
+    const input =
+    document.getElementById("userInput");
+
+    const chatBody =
+    document.getElementById("chatBody");
+
+    const userText =
+    input.value;
+
+    if(userText.trim() === "") return;
+
+
+    // USER MESSAGE
+
+    const userMessage =
+    document.createElement("div");
+
+    userMessage.className =
+    "user-message";
+
+    userMessage.textContent =
+    userText;
+
+    chatBody.appendChild(userMessage);
+
+    input.value = "";
+
+
+    // BOT MESSAGE
+
+    const botMessage =
+    document.createElement("div");
+
+    botMessage.className =
+    "bot-message";
+
+    botMessage.textContent =
+    "Thinking...";
+
+    chatBody.appendChild(botMessage);
+
+    chatBody.scrollTop =
+    chatBody.scrollHeight;
+
+
+    try{
+
+        const response = await fetch(
+
+            "/api/chat",
+
+            {
+
+                method:"POST",
+
+                headers:{
+                    "Content-Type":"application/json"
+                },
+
+                body:JSON.stringify({
+
+                    message:userText
+
+                })
+
+            }
+
+        );
+
+        const data =
+        await response.json();
+
+        botMessage.textContent =
+        data.reply;
+
+    }
+
+    catch(error){
+
+        botMessage.textContent =
+        "AI Server Error 😭";
+
+    }
+
+}
 
