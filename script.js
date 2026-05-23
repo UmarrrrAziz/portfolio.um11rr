3e543fdf7e2116a53b35691fabfd41b1a2c87e17
um11rr <140225804+UmarrrrAziz@users.noreply.github.com>
2026-05-23 10:45:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -28,64 +28,6 @@ window.addEventListener("load", function(){
 
 });
 
-
-/* AI FACE SCAN */
-
-window.addEventListener("load", () => {
-
-    const scanner =
-    document.getElementById("scanner");
-
-    const status =
-    document.getElementById("scan-status");
-
-    // AGAR ELEMENT NA MILE TOH EXIT
-    if(!scanner || !status){
-        return;
-    }
-
-    const messages = [
-
-        "Initializing AI Systems",
-        "Scanning Face",
-        "Identity Verified",
-        "Access Granted",
-        "Welcome Umar Bin Abdul Aziz 🚀"
-
-    ];
-
-    let i = 0;
-
-    status.innerText = messages[0];
-
-    const interval = setInterval(() => {
-
-        i++;
-
-        if(i < messages.length){
-
-            status.innerText =
-            messages[i];
-
-        }
-
-        else{
-
-            clearInterval(interval);
-
-            setTimeout(() => {
-
-                scanner.style.opacity = "0";
-
-                scanner.style.visibility = "hidden";
-
-            },1000);
-
-        }
-
-    },1200);
-
-});
 async function sendMessage(){
 
     const input =
